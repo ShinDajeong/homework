d3b9dc88ed6df5b7a730afb130491978eba71316
신다정 <ssdj0607@naver.com>
2020-07-21 00:02:35 +0000

    3.14 -> Math.PI 이용하여 수정

---
+++ 0720/CircleArea.java
@@ -0,0 +1,11 @@
+package com.java.first;
+
+public class CircleArea {
+
+	public static void main(String[] args) {
+		int radius = 5;
+		
+		System.out.println("반지름이 " + radius + "인 원의 넓이는 " + Math.PI * radius * radius + "Cm2입니다.");
+	}
+
+}
